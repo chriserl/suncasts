892aefa5b26ce20acfa7b799a7954c3423d6c603
chriserl <chriserldev@gmail.com>
2020-10-24 11:02:01 +0000

    restructured sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,39 +1,25 @@
 import React from "react";
 import "../sass/components/sidebar.scss";
-import closeIcon from "../assets/icons/close.svg";
-import accIcon from "../assets/icons/account.svg";
-import googleIcon from "../assets/icons/google.svg";
 
 const Sidebar = (props) => {
 	return (
 		<aside className={`sidebar ${props.sidenavVisibility}`}>
 			<nav>
-				<button onClick={props.closeSidenav} className="close-btn icon-btn">
-					<img src={closeIcon} alt="close menu" />
-				</button>
-				<a href="./" className="brand">
+				<a href="./" className="brand ph">
 					Suncasts.
 				</a>
+				<button onClick={props.closeSidenav} className="close-btn icon-btn">
+					<span className="las la-times ph small-icon"></span>
+				</button>
 			</nav>
-			<AccountPrompt />
-		</aside>
-	);
-};
-
-const AccountPrompt = () => {
-	return (
-		<div id="account-prompt">
-			<img
-				src={accIcon}
-				alt="account avatar"
-				loading="lazy"
-				className="account-icon"
-			/>
-			<div className="login-text">
-				<p className="login-prompt">You're not logged in</p>
-				<p className="login-aux">Sign in to get personalized content</p>
+			<div id="account-prompt">
+				<span className="las la-user user-avatar"></span>
+				<div className="login-text">
+					<p className="login-prompt ps">You're not logged in</p>
+					<p className="login-aux px">Sign in to get personalized content</p>
+				</div>
 			</div>
-		</div>
+		</aside>
 	);
 };
 
